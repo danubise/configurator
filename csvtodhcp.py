3227bb4d14b8dc3ce00253562fd3220fe9dd84c1
danubise <danubise@gmail.com>
2018-02-27 16:47:06 +0400

    Добавил валидацию строки во внешнюю функцию, добавил формирование таблицы для разрганичения скоростей по айпи адресам

--- csvtodhcp.py
+++ csvtodhcp.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
-
+def checkForValideLine(line):
+	if splitedLine[3] !="" and splitedLine[4] !="" and splitedLine[5] !="" and re.match("[0-9a-f]{2}([-:])[0-9a-f]{2}(\\1[0-9a-f]{2}){4}$", splitedLine[4].lower()):
+		return True
+	else:
+		return False
 def parceToDhcp(filename, skip):
 	f = open(filename,'r')
 	lineIndex = 0
@@ -10,7 +14,7 @@ def parceToDhcp(filename, skip):
 	for line in f:
 		lineIndex = lineIndex + 1
 		splitedLine = line.split(',')
-		if splitedLine[3] !="" and splitedLine[4] !="" and splitedLine[5] !="" :
+		if checkForValideLine(splitedLine):
 			config = config + "host {} {} hardware ethernet {} ; fixed-address {} ; {}\n".format(splitedLine[3].strip()
 				,chr(123), splitedLine[4].strip().lower().replace("-",":"),
 				splitedLine[5].strip(),chr(125))
@@ -19,7 +23,22 @@ def parceToDhcp(filename, skip):
 	print (config)
 	return
 def parceToIptablesLimits(filename, skip):
-	print ("Iptables")
+	# 192.168.69.199 199 6 700
+	f = open(filename,'r')
+	lineIndex = 0
+	if skip:
+		f.readline()
+		lineIndex = lineIndex + 1
+	config = ""
+	for line in f:
+		lineIndex = lineIndex + 1
+		splitedLine = line.split(',')
+		if splitedLine[3] !="" and splitedLine[4] !="" and splitedLine[5] !="" :
+			config = config + "{} {} {} {}\n".format(splitedLine[5].strip(), lineIndex,
+				splitedLine[6].strip(),splitedLine[7].strip())
+		else:
+			sys.stderr.write ("[error] The line was excluded due some error {}\n".format(lineIndex))
+	print (config)
 	return
 
 def parceToIptablesAccess():
@@ -30,7 +49,7 @@ if __name__ == "__main__":
 	import argparse
 	import os.path
 	import sys
-
+	import re
 	parser = argparse.ArgumentParser(description="CSV to dhcp and iptables parser")
 	parser.add_argument("-f","--file",required=True,
 						metavar='FILE', dest="filename", type=str, 
@@ -55,8 +74,8 @@ if __name__ == "__main__":
 
 	if args.service == "dhcp":
 		parceToDhcp(args.filename, args.includetitle)
-	elif args.service == "iptables":
-		parceToIptables(args.filename, args.includetitle)
+	elif args.service == "iptableslimits":
+		parceToIptablesLimits(args.filename, args.includetitle)
 	else:
 		print ("The --service key must has value dhcp or iptables only. You set '{}', it is incorrect".format(args.service))
 	
