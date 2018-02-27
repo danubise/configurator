ecd5dde515d40efafcc61652b5f0cb605272f37c
danubise <danubise@gmail.com>
2018-02-27 17:28:47 +0400

    Поправлены ошибки валидации, добавлен шаблон главной части конфига dhcpd, добавлен ключ -out --outfile

--- csvtodhcp.py
+++ csvtodhcp.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 def checkForValideLine(line):
-	if splitedLine[3] !="" and splitedLine[4] !="" and splitedLine[5] !="" and re.match("[0-9a-f]{2}([-:])[0-9a-f]{2}(\\1[0-9a-f]{2}){4}$", splitedLine[4].lower()):
+	if line[3] !="" and line[4] !="" and line[5] !="" and re.match("[0-9a-f]{2}([-:])[0-9a-f]{2}(\\1[0-9a-f]{2}){4}$", line[4].lower()):
 		return True
 	else:
 		return False
-def parceToDhcp(filename, skip):
-	f = open(filename,'r')
+def parceToDhcp(infilename, skip, outfilename):
+	f = open(infilename,'r')
 	lineIndex = 0
 	if skip:
 		f.readline()
@@ -20,7 +20,16 @@ def parceToDhcp(filename, skip):
 				splitedLine[5].strip(),chr(125))
 		else:
 			sys.stderr.write ("[error] The line was excluded due some error {}\n".format(lineIndex))
-	print (config)
+	if outfilename != None:
+		templateFile = open("dhcpd.header.template",'r')
+		header = templateFile.read()
+		templateFile.close()
+		ofile = open(outfilename,'w')
+		ofile.write(header)
+		ofile.write(config)
+		ofile.close()
+	else:
+		print (config)
 	return
 def parceToIptablesLimits(filename, skip):
 	# 192.168.69.199 199 6 700
@@ -51,9 +60,12 @@ if __name__ == "__main__":
 	import sys
 	import re
 	parser = argparse.ArgumentParser(description="CSV to dhcp and iptables parser")
-	parser.add_argument("-f","--file",required=True,
-						metavar='FILE', dest="filename", type=str, 
-						help="File name with host name, mac and ip address")
+	parser.add_argument("-out","--outfile",
+						metavar='FILE', dest="outfilename", type=str, 
+						help="File name for saving configuration")
+	parser.add_argument("-in","--infile",required=True,
+						metavar='FILE', dest="infilename", type=str, 
+						help="File name with host name, mac and ip address in the csv format")
 	parser.add_argument("-s", "--service",required=True, metavar='string', type=str, 
 						help="Value like a dhcp or iptable")
 	parser.add_argument("--includetitle", 
@@ -68,14 +80,15 @@ if __name__ == "__main__":
 						help="For enable the debug messages and additional information")
 	args = parser.parse_args()
 	if args.debug:
-		print ("File name value :{}".format(args.filename))
+		print ("Infile name value :{}".format(args.infilename))
 		print ("Service value :{}".format(args.service))
 		print ("Include titel value :{}".format(args.includetitle))
-
+		print ("Outfile name value :{}".format(args.outfilename))
+		
 	if args.service == "dhcp":
-		parceToDhcp(args.filename, args.includetitle)
+		parceToDhcp(args.infilename, args.includetitle, args.outfilename)
 	elif args.service == "iptableslimits":
-		parceToIptablesLimits(args.filename, args.includetitle)
+		parceToIptablesLimits(args.infilename, args.includetitle, args.outfilename)
 	else:
 		print ("The --service key must has value dhcp or iptables only. You set '{}', it is incorrect".format(args.service))
 	
